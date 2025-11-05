9d0a53188c8f489de18c1bfb48c033b1c9623723
Michael Hodges <Michael.Hodges1985@yahoo.com>
2025-11-05 10:25:22 -0600

    logo things

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import logoSvg from '../assets/mySparkleHeartSvg.svg';
 
 
 export default function NavigationBar() {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary nav-custom">
-            <Navbar.Brand href="#home">My Sparkle Heart</Navbar.Brand>
+        <Navbar expand="lg" className="nav-custom">
+            <Navbar.Brand href="#home">
+                <img
+                    src={logoSvg}
+                    alt="My Sparkle Heart"
+                    height="300"      // adjust size as needed
+                    className="d-inline-block align-top"
+                />
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto nav-item-container">
